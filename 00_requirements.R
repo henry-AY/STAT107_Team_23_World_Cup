486036f0c9f518176893591d08cc555ea4f65e38
Henry <152554805+henry-AY@users.noreply.github.com>
2025-12-05 23:39:45 -0800

    Update 00_requirements.R

--- 00_requirements.R
+++ 00_requirements.R
@@ -1,22 +1,14 @@
 # Required packages to run the repository
-library(randomForest)
-library(lubridate)
-library(forcats)
-library(stringr)
-library(purrr)
-library(tidyr)
-library(tibble)
-library(ggplot2)
-library(tidyverse)
-library(dplyr)
-library(readr)
-library(rmarkdown)
-library(knitr)
-library(corrplot)
-library(RColorBrewer)
-library(yaml)
-library(scales)
-library(gtable)
-library(glue)
-library(xfun)
-library(broom)
+packages <- c(
+  "randomForest", "lubridate", "forcats", "stringr", "purrr", "tidyr",
+  "tibble", "ggplot2", "tidyverse", "dplyr", "readr", "rmarkdown",
+  "knitr", "corrplot", "RColorBrewer", "yaml", "scales", "gtable",
+  "glue", "xfun", "broom"
+)
+
+for (pkg in packages) {
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg, repos = "https://cloud.r-project.org")
+    library(pkg, character.only = TRUE)
+  }
+}
